7ee65ebcb3fdc340966c67d51d51a5254931ff9a
jaweria-dev <123859213+jaweria-dev@users.noreply.github.com>
2023-10-05 20:31:23 +0500

    Chapter1 exercise 1.1, 1.2, 1.3, 1.4

---
+++ Assignment-01/JS/app.js
@@ -0,0 +1,116 @@
+// JavaScript Chapter:01 Assginment
+
+// ============================== Exercise 1.1 ===================================
+
+// 1.Open the browser console, type 4 + 10, and press Enter. What do you see as
+// the response?
+
+// Ans1.
+//  console.log(4+10);
+//  In console.log the response I get is 14.
+
+/*2. Use the console.log() syntax, placing a value within the rounded brackets.
+Try entering your name with quotes around it (this is to indicate the fact that
+it's a text string—we'll get to this in the next chapter).*/
+
+// Ans2.
+//  console.log("Jaweria Ahmed Siddiqui");
+
+// ============================ End of exercise 1.1 ==============================
+
+
+// ============================== Exercise 1.2 ===================================
+
+// 1. Open your code editor and create an HTML file.
+
+// Ans1. Done.
+
+//  2. Within your HTML file, set up the HTML tags, doctype, HTML, head, and
+// body, and then proceed and add the script tags.
+
+// Ans2. Done.
+
+// 3. Place some JavaScript code within the script tags. You can use console.
+// log("hello world!").
+
+// Ans3. Done in index.html
+
+// ============================ End of exercise 1.2 ==============================
+
+
+// ============================== Exercise 1.3 ===================================
+
+// 1. Create a separate file called app with the extension .js.
+
+// Ans1. Done
+
+// 2. Within the .js file, add some JavaScript code.
+
+// Ans2.
+// console.log("Hello JavaScript");
+
+// 3. Link to the separate .js file within the HTML file you created in Practice
+// exercise 1.2.
+
+// Ans3. Done
+// console.log("hello world!");
+
+// 4. Open the HTML file within your browser and check to see whether the
+// JavaScript code ran properly.
+
+// Ans4. yes it's run properly.
+
+// ============================ End of exercise 1.3 ==============================
+
+
+// ============================== Exercise 1.4 ===================================
+
+/*1. Add a new statement to your JavaScript code by setting a variable value.
+Since we will cover this in the next chapter, you can use the following line:
+let a = 10;*/
+
+// Ans1. 
+// let a = 10;
+// console.log("The value of a is " + a);
+
+
+// 2. Add a comment at the end of the statement indicating that you set a value of
+// 10.
+
+// Ans2.
+// let a = 10; // Set the value of a is 10
+
+
+// 3. Print the value using console.log(). Add a comment explaining what this
+// will do.
+
+// Ans3.
+// Example1
+// let a = 10;
+// console.log("The value of a is " + a); // console.log display the value of variable a.
+
+// Example2
+// console.log("jiya") // here console.log shows my nick name.
+
+
+/* 4. At the end of your JavaScript code, use a multiple-line comment. In a real
+ production script, you might use this space to add a brief outline of the
+ purpose of the file.*/
+
+// Ans4
+// Example1
+// let nam = "Jaweria"
+// console.log("Hello! my name is " + nam);
+
+/* In this code i set a variable nam assgin my real name jaweria in string bcz this is not a number so i used this in a string & print console.log hello my name is nam (i already assgin the variable name before which is jaweria so in console.log variable nam print jaweria)*/
+
+
+// Example2
+// let x = 50;
+// console.log("The value of x is " + x);
+
+/* In this code i set a variable which i assgin a value 50 & this is a number (50) so we donot write it in a string & in next line print it to a console the value of x is 50*/
+
+
+
+// ============================ End of exercise 1.4 ==============================
